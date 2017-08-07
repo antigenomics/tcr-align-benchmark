4bcf13a6cd45b1d31642ef1dbb2006e2092b06b5
glennerad <glennerad@gmail.com>
2017-08-07 16:28:41 +0300

    no message

--- ch_scripts/ch_compute_distances.py
+++ ch_scripts/ch_compute_distances.py
@@ -1,3 +1,29 @@
+"""
+This file was exported from tcr-dist package with some modifications
+
+MIT License
+
+Copyright (c) 2017 Philip Harlan Bradley and Jeremy Chase Crawford
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 ## this script will compute distances between all the tcrs and write out a distance matrix
 ## the order of rows and columns in the distance matrix will match that in the input file
 ## it will also compute the rank scores for each tcr with respect to all the epitopes present
